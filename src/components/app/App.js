6cec2e974eb5b4bce31be708c67a7481712b660e
sibel <sibel10789@gmail.com>
2022-01-20 16:41:12 +0300

    second commit

---
+++ src/components/app/App.js
@@ -0,0 +1,43 @@
+// import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <aside>
+        <h1>
+          ShopName
+        </h1>
+        <p>
+          Магазин <br></br>
+          ID 0000sadf0sdaf0asdf
+        </p>
+        <ul>
+          <li>Обзор</li>
+          <li>Дашборд</li>
+          <li>Заказы</li>
+          <li>Карточки товаров</li>
+          <li>Управление расходами</li>
+          <li>Управление доступом</li>
+          <li><b>Калькулятор</b></li>
+        </ul>
+      </aside>
+      <header className="App-header">
+        <img src=""className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
