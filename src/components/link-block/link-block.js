7c9da581e00dbeb11646ae5ad0e5999df25f1e66
rusibel <sergiibilous@gmail.com>
2022-02-12 17:58:13 +0300

    final 21

--- src/components/link-block/link-block.js
+++ src/components/link-block/link-block.js
@@ -7,15 +7,12 @@ export default function LinkBlock () {
             <div>
                 <div className="link__block__item">
                     <span><b>Категория на OZON и комиссия </b></span>
-                    {/* <a href="https://calculator.ozon.ru">Калькулятор OZON</a> */}
                 </div>
                 <div className="link__block__item">
                     <span><b>Категория на WB и комиссия      </b></span>
-                    {/* <a href="https://seller.wildberries.ru/dynamic-product-categories">Калькулятор WILDBERRIES</a> */}
                 </div>
                 <div className="link__block__item">
                     <span><b>Комиссии на Яндекс.Маркете   </b></span>
-                    {/* <a href="https://yandex.ru/legal/marketplace_services_rate_table/">Комиссии Яндекс.Маркет</a> */}
                 </div>
             </div>
             <div>
